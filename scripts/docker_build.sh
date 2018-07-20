d5eedd61e3f31dae29d29f2d483c80338dcc4a80
night-crawler <lilo.panic@gmail.com>
2018-07-20 16:14:39 +0300

    Bumped version: 0.1.4 → 0.1.5

--- scripts/docker_build.sh
+++ scripts/docker_build.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 docker build -t ncrawler/msa_mailer:latest \
-             -t ncrawler/msa_mailer:0.1.4 \
+             -t ncrawler/msa_mailer:0.1.5 \
              .
 
 #docker-squash -t ncrawler/msa_mailer:latest-squashed ncrawler/msa_mailer:latest
-#docker tag ncrawler/msa_mailer:latest-squashed ncrawler/msa_mailer:0.1.4-squashed
+#docker tag ncrawler/msa_mailer:latest-squashed ncrawler/msa_mailer:0.1.5-squashed
