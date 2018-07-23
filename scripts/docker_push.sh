cada8d287c848a28df97619ecd3d81be0af360b1
night-crawler <lilo.panic@gmail.com>
2018-07-23 16:34:39 +0300

    Bumped version: 0.1.7 → 0.1.8

--- scripts/docker_push.sh
+++ scripts/docker_push.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 #docker push ncrawler/msa_mailer:latest-squashed
-#docker push ncrawler/msa_mailer:0.1.7-squashed
+#docker push ncrawler/msa_mailer:0.1.8-squashed
 docker push ncrawler/msa_mailer:latest
-docker push ncrawler/msa_mailer:0.1.7
+docker push ncrawler/msa_mailer:0.1.8
