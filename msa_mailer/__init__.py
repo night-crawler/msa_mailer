f2e1ca015774e1efb622c2fadab2678216c26ad2
night-crawler <lilo.panic@gmail.com>
2018-07-23 16:04:16 +0300

    Bumped version: 0.1.6 → 0.1.7

--- msa_mailer/__init__.py
+++ msa_mailer/__init__.py
@@ -1,3 +1,3 @@
 from .celery import app as celery_app
 
-__version__ = '0.1.6'
+__version__ = '0.1.7'
--- scripts/docker_build.sh
+++ scripts/docker_build.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 docker build -t ncrawler/msa_mailer:latest \
-             -t ncrawler/msa_mailer:0.1.6 \
+             -t ncrawler/msa_mailer:0.1.7 \
              .
 
 #docker-squash -t ncrawler/msa_mailer:latest-squashed ncrawler/msa_mailer:latest
-#docker tag ncrawler/msa_mailer:latest-squashed ncrawler/msa_mailer:0.1.6-squashed
+#docker tag ncrawler/msa_mailer:latest-squashed ncrawler/msa_mailer:0.1.7-squashed
